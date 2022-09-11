a9c736a7a616f0c58a749a2be1454b2c7a2093f4
Stig Hornang <shornang@gmail.com>
2022-09-11 12:58:28 +0200

    fix: Ignore accidental PNGs in source tree when building ico

--- create_icon.sh
+++ create_icon.sh
@@ -10,16 +10,25 @@ FILENAME=$(basename -- "$INPUT")
 EXTENSION="${FILENAME##*.}"
 FILENAME="${FILENAME%.*}"
 
-for WIDTH in 16 32 44 64 128 150 256
+WIDTHS=(16 32 44 64 128 150 256)
+
+for WIDTH in "${WIDTHS[@]}"
 do
     rsvg-convert --output="${FILENAME}_${WIDTH}.png" -w $WIDTH "$INPUT"
 done
 
+PNGS=()
+for WIDTH in "${WIDTHS[@]}"; do
+    PNGS+=("${FILENAME}_${WIDTH}.png")
+done
+
+PNGS_ARG="${PNGS[@]}"
+
 if [[ "$IMAGEMAGICK" == *convert ]]; then
     # We don't call use subcommand on a system with the legacy "convert" executable
-    convert *.png "$OUTPUT"
+    convert $PNGS_ARG "$OUTPUT"
 else
-    "$IMAGEMAGICK" convert *.png "$OUTPUT"
+    "$IMAGEMAGICK" convert $PNGS_ARG "$OUTPUT"
 fi
 
 
