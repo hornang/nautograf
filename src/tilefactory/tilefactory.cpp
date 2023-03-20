hornang/nautograf

commit 5a9cbb9c3bddcfb40876ac679a4c8c02889f52bb

feat: Render charts after all have been loaded
Avoid CPU intensive loading progress by not trigger rendering until all
charts' (headers) have been loaded.

The chart loading step should be removed completely via caching in the
future.

src/tilefactory/tilefactory.cpp
@@ -228,36 +228,33 @@ bool TileFactory::hasSource(const std::string &name)
     return false;
 }
 
-void TileFactory::insertSorted(const TileFactory::Source &source)
+void TileFactory::setSources(const std::vector<TileFactory::Source> &sources)
 {
     const std::lock_guard<std::mutex> lock(m_sourcesMutex);
-    auto upper = std::upper_bound(m_sources.begin(),
-                                  m_sources.end(),
-                                  source, [](const TileFactory::Source &a, const TileFactory::Source &b) -> bool {
-                                      return a.tileSource->scale() < b.tileSource->scale();
-                                  });
-
-    m_sources.insert(upper, source);
-}
-
-void TileFactory::addSources(const std::vector<TileFactory::Source> &sources)
-{
+    std::vector<TileFactory::Source> qualifiedSources;
     std::vector<GeoRect> rois;
+
+    for (const auto &source : m_sources) {
+        rois.push_back(source.tileSource->extent());
+    }
 
     for (const auto &source : sources) {
         if (!source.tileSource) {
             std::cerr << "Not adding null source" << std::endl;
             continue;
         }
-        if (hasSource(source.name)) {
-            std::cerr << "Source with name " << source.name << " already exists " << std::endl;
-            continue;
-        }
+        qualifiedSources.push_back(source);
         rois.push_back(source.tileSource->extent());
-        insertSorted(source);
     }
 
     m_previousTileLocations.clear();
+    m_sources = sources;
+
+    std::sort(m_sources.begin(),
+              m_sources.end(),
+              [](const TileFactory::Source &a, const TileFactory::Source &b) -> bool {
+                  return a.tileSource->scale() < b.tileSource->scale();
+              });
 
     if (m_updateCallback) {
         m_updateCallback();

src/chartmodel.cpp
@@ -152,10 +152,6 @@ void ChartModel::addSource(const std::shared_ptr<OesencTileSource> &tileSource, 
     sourceWrapper.encrypted = encrypted;
     sourceWrapper.source = source;
 
-    if (tileSource) {
-        m_tileFactory->addSources(std::vector<TileFactory::Source>({ source }));
-    }
-
     for (auto it = m_sourceCache.begin(); it < m_sourceCache.end(); it++) {
         if (it->source.name > source.name) {
             int i = std::distance(m_sourceCache.begin(), it);
@@ -178,6 +174,11 @@ bool ChartModel::loadNextFromQueue()
         m_loadingCharts = false;
         m_loadingProgress = 1;
         emit loadingProgressChanged();
+        std::vector<TileFactory::Source> sources;
+        for (const SourceWrapper &sourceWrapper : m_sourceCache) {
+            sources.push_back(sourceWrapper.source);
+        }
+        m_tileFactory->setSources(sources);
         return true;
     }
 

src/tilefactory/include/tilefactory/tilefactory.h
@@ -61,13 +61,12 @@ public:
 
     void setUpdateCallback(std::function<void(void)> updateCallback) { m_updateCallback = updateCallback; }
     void setChartsChangedCb(std::function<void(std::vector<GeoRect> roi)> chartsChangedCb) { m_chartsChangedCb = chartsChangedCb; }
-    void addSources(const std::vector<TileFactory::Source> &sources);
+    void setSources(const std::vector<TileFactory::Source> &sources);
     void setChartEnabled(const std::string &name, bool enabled);
     std::vector<int> setAllChartsEnabled(bool enabled);
     std::vector<Source> sources() const { return m_sources; }
 
 private:
-    void insertSorted(const TileFactory::Source &source);
     bool hasSource(const std::string &id);
     static std::vector<GeoRect> tilesInViewport(const GeoRect &rect, int zoom);
     static std::string tileId(const GeoRect &rectBox, int pixelsPerLongitude);

commit c7e28674b1d303303968ff94537099ed840450dd

fix: Fix crash when loading dir containing invalid chart
This happens when loading a directory with a chart that could not be
loaded (either as encrypted or decrypted chart). Bug introduced in
5a9cbb9c.

src/tilefactory/tilefactory.cpp
@@ -295,7 +295,7 @@ void TileFactory::setSources(const std::vector<TileFactory::Source> &sources)
     }
 
     m_previousTileLocations.clear();
-    m_sources = sources;
+    m_sources = qualifiedSources;
 
     std::sort(m_sources.begin(),
               m_sources.end(),