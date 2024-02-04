31184d3b68b5914b64c28741b6ae1ddd1f88ed59
Stig Hornang <shornang@gmail.com>
2024-02-04 16:33:38 +0100

    feat(scene): Generate font atlas in the background
    Avoid stalling application startup when font atlas is generated.
    Instead update the scene once the glyphs are available.
    
    Should probably handle "no font atlas available" case better.

--- src/scene/include/scene/fontimage.h
+++ src/scene/include/scene/fontimage.h
@@ -3,18 +3,25 @@
 #include <QDir>
 #include <QImage>
 #include <QList>
+#include <QObject>
 #include <QRectF>
 #include <QSize>
 #include <QString>
 
+#include <optional>
+
 #include <msdf-atlas-gen/msdf-atlas-gen.h>
 #include <msdf-atlas-read.h>
 
-class FontImage
+class FontWorker;
+
+class FontImage : public QObject
 {
+    Q_OBJECT
 
 public:
     FontImage();
+    ~FontImage();
     FontImage(const FontImage &other) = delete;
 
     enum class FontType {
@@ -23,16 +30,37 @@ public:
         Normal,
     };
 
-    const QImage &image() const { return m_image; }
+    struct Atlas
+    {
+        QImage image;
+        std::unordered_map<FontType, msdf_atlas_read::FontGeometry> geometries;
+    };
+
+    QImage image() const
+    {
+        if (!m_atlas.has_value()) {
+            return {};
+        }
+        return m_atlas->image;
+    }
     QList<msdf_atlas_read::GlyphMapping> glyphs(const QString &text, float pixelSize, FontType type = FontType::Soundings) const;
-    QSize atlasSize() const { return m_image.size(); }
+    QSize atlasSize() const
+    {
+        if (!m_atlas.has_value()) {
+            return {};
+        }
+        return m_atlas->image.size();
+    }
     QRectF boundingBox(const QString &text, float pixelSize, FontType type = FontType::Soundings) const;
     static QString locateFontFile(FontType type);
 
+signals:
+    void atlasChanged();
+
+public slots:
+    void setAtlas(const FontImage::Atlas &cache);
+
 private:
-    bool createCache(const QDir &dir);
-    bool loadCache(const QDir &dir);
-    QImage m_image;
-    msdf_atlas_read::AtlasProperties m_atlasProperties;
-    std::unordered_map<FontType, msdf_atlas_read::FontGeometry> m_fontGeometries;
+    FontWorker *m_worker = nullptr;
+    std::optional<FontImage::Atlas> m_atlas;
 };
