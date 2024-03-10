hornang/nautograf

commit 30f06b97cbb83a5be6f5c32aa056a49b15b695ee

feat: Add class for font rendering using freetype-gl

src/scene/fontimage.cpp
@@ -1,0 +1,161 @@
+#include <freetype-gl/freetype-gl.h>
+#include <freetype-gl/texture-font.h>
+
+#include <QDebug>
+#include <QDir>
+#include <QFile>
+#include <QImage>
+#include <QPainter>
+#include <QRawFont>
+#include <QRectF>
+#include <QSGTexture>
+#include <QStandardPaths>
+
+#include "fontimage.h"
+
+constexpr float basePointSize = 20;
+
+FontImage::FontImage()
+{
+    const QList<FontType> fontsToLoad = { FontType::Normal,
+                                          FontType::Soundings };
+
+    constexpr int atlasWidth = 512;
+    constexpr int atlasHeight = 512;
+    m_textureAtlas = texture_atlas_new(atlasWidth, atlasHeight, 1);
+
+    const QString glyphsToRender(R"(ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅabcdefghijklmnopqrstuvwxyz1234567890æøå.,-? )");
+
+    for (const auto &fontType : fontsToLoad) {
+        const auto fontFile = locateFontFile(fontType);
+
+        if (fontFile.isEmpty()) {
+            qWarning() << "Unable to find a system font";
+            continue;
+        }
+
+        qInfo() << "Found font file: " << fontFile;
+
+        auto textureFont = texture_font_new_from_file(m_textureAtlas,
+                                                      basePointSize,
+                                                      fontFile.toUtf8().data());
+
+        if (!textureFont) {
+            qWarning() << "Failed to create texture font";
+            continue;
+        }
+
+        textureFont->rendermode = RENDER_SIGNED_DISTANCE_FIELD;
+        QHash<QChar, struct texture_glyph_t *> glyphs;
+
+        for (const QChar &c : glyphsToRender) {
+            QString s = c;
+            // I guess this functions reads as many bytes it needs to get a valid UTF-8 char
+            glyphs[c] = texture_font_get_glyph(textureFont, s.toUtf8().data());
+        }
+
+        m_glyphs[fontType] = { fontType, textureFont, glyphs };
+    }
+
+    QImage image(m_textureAtlas->data, atlasWidth, atlasHeight, QImage::Format_Grayscale8);
+    m_image = image;
+}
+
+QRectF FontImage::boundingBox(const QString &text, float pointSize, FontType type) const
+{
+    int yMin = std::numeric_limits<int>::max();
+    int yMax = std::numeric_limits<int>::min();
+    int xMax = std::numeric_limits<int>::min();
+
+    auto glyphs = FontImage::glyphs(text, pointSize, type);
+
+    for (const auto & glyph: glyphs) {
+        yMin = std::min<float>(glyph.target.top(), yMin);
+        yMax = std::max<float>(glyph.target.bottom(), yMax);
+        xMax = std::max<float>(glyph.target.right(), xMax);
+    }
+
+    return QRectF(0, yMin, xMax, yMax - yMin);
+}
+
+QList<FontImage::Glyph> FontImage::glyphs(const QString &text,
+                                          float pointSize,
+                                          FontType type) const
+{
+    if (!m_glyphs.contains(type)) {
+        return {};
+    }
+
+    float advance = 0;
+    QString prevChar = 0;
+    const float ratio = pointSize / basePointSize;
+    QList<FontImage::Glyph> glyphs;
+
+    for (int i = 0; i < text.length(); i++) {
+        texture_glyph_t *glyph = m_glyphs[type].glyphs[text.at(i)];
+
+        if (!glyph) {
+            continue;
+        }
+
+        QPointF topLeft(advance + glyph->offset_x * ratio, -glyph->offset_y * ratio);
+        QPointF bottomRight = topLeft + QPointF(glyph->width * ratio, glyph->height * ratio);
+        QRectF texture(QPointF(glyph->s0, glyph->t0), QPointF(glyph->s1, glyph->t1));
+
+        Glyph outputGlyph {
+            texture,
+            QRectF(topLeft, bottomRight)
+        };
+
+        glyphs.append(outputGlyph);
+        advance += (glyph->advance_x + texture_glyph_get_kerning(glyph, prevChar.toUtf8().data())) * ratio;
+        prevChar = text.at(i);
+    }
+
+    return glyphs;
+}
+
+QString FontImage::locateFontFile(FontType type)
+{
+#ifdef Q_OS_LINUX
+    QDir dir("/usr/share/fonts/truetype/ubuntu");
+
+    QStringList fonts;
+    switch (type) {
+    case FontType::Normal:
+        fonts = dir.entryList({ "ubuntu-r*" });
+        break;
+    case FontType::Soundings:
+        fonts = dir.entryList({ "ubuntu-ri*" });
+        break;
+    }
+#else
+    QDir dir(R"(C:\Windows\Fonts)");
+
+    QStringList fonts;
+    switch (type) {
+    case FontType::Normal:
+        fonts = dir.entryList({ "arial*" });
+        break;
+    case FontType::Soundings:
+        fonts = dir.entryList({ "ariali*" });
+        break;
+    }
+#endif
+    if (fonts.isEmpty()) {
+        return {};
+    }
+
+    return dir.filePath(fonts.first());
+}
+
+FontImage::~FontImage()
+{
+    for (const FontGlyphs &fontGlyphs : qAsConst(m_glyphs)) {
+        texture_font_delete(fontGlyphs.textureFont);
+    }
+
+    if (m_textureAtlas) {
+        texture_atlas_delete(m_textureAtlas);
+    }
+}

CMakeLists.txt
@@ -16,6 +16,8 @@ find_package(Qt6
 set(MSDFGEN_BUILD_STANDALONE false)
 add_subdirectory(msdfgen)
 
+find_package(freetype-gl CONFIG REQUIRED)
+
 find_program(IMAGEMAGICK
     NAMES magick convert
 )
@@ -72,6 +74,8 @@ add_executable(nautograf WIN32
     src/chartmodel.h
     src/usersettings.cpp
     src/usersettings.h
+    src/scene/fontimage.cpp
+    src/scene/fontimage.h
     src/scene/symbolimage.cpp
     src/scene/symbolimage.h
     ${QML_RESOURCES}
@@ -109,6 +113,7 @@ target_link_libraries(nautograf
     tilefactory
     msdfgen::msdfgen-core
     msdfgen::msdfgen-ext
+    freetype-gl
 )
 
 if(WIN32)

src/scene/fontimage.h
@@ -1,0 +1,47 @@
+#pragma once
+
+#include <QImage>
+
+class QSGTexture;
+
+namespace ftgl {
+struct texture_atlas_t;
+struct texture_font_t;
+}
+
+class FontImage
+{
+
+public:
+    FontImage();
+    ~FontImage();
+    FontImage(const FontImage &other) = delete;
+
+    enum class FontType {
+        Soundings,
+        Normal,
+    };
+
+    struct Glyph
+    {
+        QRectF texture;
+        QRectF target;
+    };
+
+    QImage image() const { return m_image; }
+    QList<Glyph> glyphs(const QString &text, float pixelSize, FontType type = FontType::Soundings) const;
+    QRectF boundingBox(const QString &text, float pixelSize, FontType type = FontType::Soundings) const;
+
+private:
+    struct FontGlyphs
+    {
+        FontType type;
+        struct texture_font_t *textureFont;
+        QHash<QChar, struct texture_glyph_t *> glyphs;
+    };
+
+    static QString locateFontFile(FontType type);
+    QImage m_image;
+    QHash<FontType, FontGlyphs> m_glyphs;
+    struct texture_atlas_t *m_textureAtlas = nullptr;
+};

commit e72300bc7fee4159f89da141c0e395f04200b0eb

fix(scene): Fix bounding box for zero length string
Bug introduced in 30f06b97 and later triggered in e31ffe6c where empty
string could be passed to FontImage::boundingBox().

The bug created ghost labels (due to large negative sized rectangles)
that would cause real labels to disappear because of the collision
checking.

src/scene/fontimage.cpp
@@ -234,6 +234,10 @@ void FontImage::setAtlas(const FontImage::Atlas &atlas)
 
 QRectF FontImage::boundingBox(const QString &text, float pointSize, FontType type) const
 {
+    if (text.isEmpty()) {
+        return {};
+    }
+
     int yMin = std::numeric_limits<int>::max();
     int yMax = std::numeric_limits<int>::min();
     int xMax = std::numeric_limits<int>::min();

src/scene/test/fontimage_test.cpp
@@ -1,3 +1,5 @@
+#include <QString>
+
 #include <cmath>
 #include <gtest/gtest.h>
 
@@ -8,4 +10,11 @@ TEST(FontImageTest, SaveAtlasToFile)
     FontImage fontImage;
     const QImage &image = fontImage.image();
     image.save("atlas.png");
+}
+
+TEST(FontImageTest, EmptyTextGivesZeroBoundingBox)
+{
+    FontImage fontImage;
+    QRectF boundingBox = fontImage.boundingBox(QString(), 12);
+    ASSERT_TRUE(boundingBox.isNull());
 }